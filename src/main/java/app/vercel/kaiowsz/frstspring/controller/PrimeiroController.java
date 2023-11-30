07c04a8d306d8b5c5fbbd989aea7380b7f7e6817
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2023-11-30 09:29:28 -0300

    controller

--- src/main/java/app/vercel/kaiowsz/frstspring/controller/PrimeiroController.java
+++ src/main/java/app/vercel/kaiowsz/frstspring/controller/PrimeiroController.java
@@ -2,10 +2,13 @@ package app.vercel.kaiowsz.frstspring.controller;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +40,24 @@ public class PrimeiroController {
         return "Your username: " + usuario.username + ". Your age: " + usuario.idade;
     }
 
-    record Usuario(String username, int idade) {}
+    @PostMapping("/fifth")
+    public String fifth(@RequestHeader Map<String, String> headers) {
+        return "Your username: " + headers;
+    }
+
+    @GetMapping("/sixth/{id}")
+    public ResponseEntity<Object> sixth(@PathVariable long id) {
+
+        var usuario = new Usuario("kaioba", 15);
+
+        if(id > 5) {
+            return ResponseEntity.status(HttpStatus.OK).body(usuario);
+        }
+
+        return ResponseEntity.badRequest().body("Something went wrong");
+    }
+
+    record Usuario(String username, int idade) {
+
+    }
 }   
